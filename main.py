9bfc056daed281da4d9ded151122bc94defd8216
thisiskeanyvy <notifkeanyvy@gmail.com>
2022-10-07 20:25:07 +0200

    define encode and decode function (morse code)

--- main.py
+++ main.py
@@ -4,24 +4,21 @@ This educational project is a POC to test different Air-Gap attack vectors.
 (Acoustic, optical, thermal, vibrational, magnetic and electric)
 """
 
-keys = {"0":"-----","1":".----","2":"..---","3":"...--","4":"....-","5":".....","6":"-....","7":"--...","8":"---..","9":"----.","a":".-","b":"-...","c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--..","/":"-..-.",".":".-.-.-","!":"--..--","?":"..--..","=":"-...-","&":".-...","@":".--.-.","_":"..--.-","kn":"-.--.","sk":"...-.-","ar":".-.-.","bk": "-...-.-"}
+encode_keys = {"0":"-----","1":".----","2":"..---","3":"...--","4":"....-","5":".....","6":"-....","7":"--...","8":"---..","9":"----.","a":".-","b":"-...","c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--..","/":"-..-.",".":".-.-.-","!":"--..--","?":"..--..","=":"-...-","&":".-...","@":".--.-.","_":"..--.-","kn":"-.--.","sk":"...-.-","ar":".-.-.","bk": "-...-.-"," ": "SPACE"}
+
+decode_keys = {v: k for k, v in encode_keys.items()}
 
 def encode():
     secret = input("Encode : ")
-    final_secret = ""
-    for k in range(len(secret)):
-        if secret[k] != " ":
-            final_secret += keys[secret[k]] + keys["@"]
-        else:
-            final_secret += keys["@"] + keys["@"]
+    final_secret = " ".join(encode_keys[x] for x in secret)
+    final_secret = final_secret.replace(" SPACE ", "   ")
     print(final_secret)
 
-def decode(): #unfinished
+def decode():
     secret = input("Decode : ")
-    secret = secret.replace(".--.-.","@")
-    for k,s in keys.items():
-        secret = secret.replace(s,k)
-    print(secret)
+    secret = secret.replace("   ", " SPACE ").split(" ")
+    final_secret = "".join(decode_keys[x] for x in secret)
+    print(final_secret)
 
 if __name__ == "__main__":
     encode()
